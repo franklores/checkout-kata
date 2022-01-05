b11be6b9efc51b3a6cbd24f0d068de9c143636ba
Frank Lores-Penalver <frank@lores.email>
2022-01-05 00:55:46 +0000

    add checkout service

---
+++ Application/Services/Checkout.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.Services;
+
+using Application.Interfaces;
+
+using Domain;
+
+using System;
+using System.Threading.Tasks;
+
+public class Checkout : ICheckout
+{
+    public Task<decimal> GetTotalPrice()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task ScanItem(Item item)
+    {
+        throw new NotImplementedException();
+    }
+}
